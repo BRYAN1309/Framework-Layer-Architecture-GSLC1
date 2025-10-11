ee81c2326941e8c753aab75346d8e21145b46aac
BRYAN1309 <bryanthanaya1@gmail.com>
2025-10-11 20:34:50 +0700

    project structure

---
+++ prototype/src/test/java/com/firstconnection/prototype/PrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstconnection.prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
